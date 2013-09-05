9f49a3f4fe2df72a10be8ee9c6b22626592777d0
lokeshjetty <lokesh.jetty@gmail.com>
2013-09-05 22:38:10 +0530

    Added ssjs

---
+++ web.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var fs = require('fs');
+var buffer;
+
+var app = express.createServer(express.logger());
+
+app.use(express.static(__dirname+"/"));
+
+app.get('/', function(request, response) {
+//  response.send('Hello World2!');
+    buffer = fs.readFileSync('index.html');
+    response.send(buffer.toString());
+});
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
